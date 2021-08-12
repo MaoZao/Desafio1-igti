fa8ce2c4bd6e4c95f1cffd58e6f4c30c6446910d
MaoZao <salomao.3@gmail.com>
2021-08-12 16:43:52 -0300

    s3_files

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket_object" "job_spark_delta" {
   bucket = aws_s3_bucket.dl.id
-  key    = "emr-code/pyspark/job_spark_from_tf.py"
+  key    = "emr-code/pyspark/job_spark_delta.py"
   acl    = "private"
   source = "../job_spark_delta.py"
   etag   = filemd5("../job_spark_delta.py")
