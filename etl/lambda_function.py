5a62aa4707de8aa5f1a097c286436f8b323c1b0d
MaoZao <salomao.3@gmail.com>
2021-08-12 16:30:31 -0300

    subida yaml

---
+++ etl/lambda_function.py
@@ -0,0 +1,99 @@
+import boto3
+
+def handler(event, context):
+    """
+    Lambda function that starts a job flow in EMR.
+    """
+    client = boto3.client('emr', region_name='us-east-2')
+
+    cluster_id = client.run_job_flow(
+                Name='EMR-Psalomao-IGTI-delta',
+                ServiceRole='EMR_DefaultRole',
+                JobFlowRole='EMR_EC2_DefaultRole',
+                VisibleToAllUsers=True,
+                LogUri='s3://datalake-psalomao-155914520574/emr-logs',
+                ReleaseLabel='emr-6.3.0',
+                Instances={
+                    'InstanceGroups': [
+                        {
+                            'Name': 'Master nodes',
+                            'Market': 'SPOT',
+                            'InstanceRole': 'MASTER',
+                            'InstanceType': 'm5.xlarge',
+                            'InstanceCount': 1,
+                        },
+                        {
+                            'Name': 'Worker nodes',
+                            'Market': 'SPOT',
+                            'InstanceRole': 'CORE',
+                            'InstanceType': 'm5.xlarge',
+                            'InstanceCount': 1,
+                        }
+                    ],
+                    'Ec2KeyName': 'psalomao-igti-teste',
+                    'KeepJobFlowAliveWhenNoSteps': True,
+                    'TerminationProtected': False,
+                    'Ec2SubnetId': 'subnet-59cce715'
+                },
+
+                Applications=[
+                    {'Name': 'Spark'}
+                ],
+
+                Configurations=[{
+                    "Classification": "spark-env",
+                    "Properties": {},
+                    "Configurations": [{
+                        "Classification": "export",
+                        "Properties": {
+                            "PYSPARK_PYTHON": "/usr/bin/python3",
+                            "PYSPARK_DRIVER_PYTHON": "/usr/bin/python3"
+                        }
+                    }]
+                },
+                    {
+                        "Classification": "spark-hive-site",
+                        "Properties": {
+                            "hive.metastore.client.factory.class": "com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory"
+                        }
+                    },
+                    {
+                        "Classification": "spark-defaults",
+                        "Properties": {
+                            "spark.submit.deployMode": "cluster",
+                            "spark.speculation": "false",
+                            "spark.sql.adaptive.enabled": "true",
+                            "spark.serializer": "org.apache.spark.serializer.KryoSerializer"
+                        }
+                    },
+                    {
+                        "Classification": "spark",
+                        "Properties": {
+                            "maximizeResourceAllocation": "true"
+                        }
+                    }
+                ],
+                
+                StepConcurrencyLevel=1,
+                
+                Steps=[{
+                    'Name': 'Delta Insert do Censo Escolar',
+                    'ActionOnFailure': 'CONTINUE',
+                    'HadoopJarStep': {
+                        'Jar': 'command-runner.jar',
+                        'Args': ['spark-submit',
+                                 '--packages', 'io.delta:delta-core_2.12:1.0.0', 
+                                 '--conf', 'spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension', 
+                                 '--conf', 'spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog', 
+                                 '--master', 'yarn',
+                                 '--deploy-mode', 'cluster',
+                                 's3://datalake-psalomao-155914520574/emr-code/pyspark/job_spark_delta.py'
+                                 ]
+                    }
+                }],
+            )
+    
+    return {
+        'statusCode': 200,
+        'body': f"Started job flow {cluster_id['JobFlowId']}"
+    }
